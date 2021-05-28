b2e431bf1d5681584b087ca7064de5f1938fbfb2
Len <len@litigaze.com>
2021-05-28 19:36:34 +1000

    add test slider

--- tools.js
+++ tools.js
@@ -1,1 +1,21 @@
-//Test
+$(function() {
+
+    var sliderwrapper = $('.slider-wrapper');
+    //Set element IDs for noUiSlider
+    sliderwrapper.children().remove();//Remove children from slider wrapper
+    sliderwrapper.append('<div id="slider"></div>');
+    var slider = document.getElementById('zoomslider');
+    //Create sliders based on events number
+    //Create slider
+    noUiSlider.create(slider, {
+    start: 0,
+    step: 1,
+    range: {
+        'min': 0,
+        'max': 100
+    },
+    keyboardSupport: true
+    })
+
+
+});//END
